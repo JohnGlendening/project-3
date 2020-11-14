4b64e58dabca0e6b4bfb3350e8d0ea88f00f6749
John Glendening <66345555+JohnGlendening@users.noreply.github.com>
2020-11-14 11:17:40 -0600

    Add files via upload

---
+++ pages/about.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import Navbar from '../components/navbar';
+import Jumbotron from '../components/jumbotron';
+import Footer from '../components/footer';
+import IconImage from '../components/iconImage';
+import '../static/css/pages.css';
+
+const App = () => (
+  <div>
+    <Navbar />
+    <Jumbotron phrase="to your health" />
+    <h3>about</h3>
+    <hr />
+    <div className="row">
+      <div className="col-md-6 aboutSections">
+        <img className="profilePic profilePic1" src=".jpg" alt="profilepic" />
+      </div>
+      <div className="col-md-6 aboutSections">
+        <h4 className="bio bio1">
+        Talk about site
+        </h4>
+      </div>
+    </div>
+    <h3 className="tools">Tools</h3>
+    <hr />
+    <div className="row">
+      <div className="col">
+        <div className="row">
+          <div className="col-sm-6">
+            <IconImage source="../static/pics/html.png" alt="html" />
+            <IconImage source="../static/pics/bootstrap.png" alt="bootstrap" />
+            <IconImage source="../static/pics/css.png" alt="css" />
+          </div>
+          <div className="col-sm-6">
+            <IconImage source="../static/pics/githubIcon.png" alt="github" />
+            <IconImage source="../static/pics/handlebars.png" alt="handlebars" />
+            <IconImage source="../static/pics/javascript.png" alt="javascript" />
+          </div>
+    </div>
+    <Footer />
+  </div>
+);
+
+export default App;
